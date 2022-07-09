3b28b01a0df44ed5ccb48ab87755f7918e9f76ee
ali moniri <alimoniri81@gmail.com>
2022-07-09 14:13:57 +0430

    category horzintal scroll added (mobile)

--- js/app.js
+++ js/app.js
@@ -18,3 +18,16 @@ document.getElementById('burger').addEventListener('click', function() {
 document.getElementById('close-button').addEventListener('click', function() {
     document.getElementById('mobile-menu').classList.remove('dis-block')
 })
+
+//category scroll /-------------------------
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 350) {
+        document.getElementById("category-scroll").classList.add('visible');
+        document.getElementById("category-scroll").classList.remove('invisible');
+
+    } else {
+        document.getElementById("category-scroll").classList.remove('visible');
+        document.getElementById("category-scroll").classList.add('invisible');
+    }
+
+}
